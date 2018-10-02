e8db6ac5074a52e7fdc44073b49ffc3edf08aa71
zachvalenta <zjv5002@gmail.com>
2018-10-01 22:50:21 -0400

    add - binary search

--- src/search.py
+++ src/search.py
@@ -1,13 +1,11 @@
-"""
-source: Grokking Algorithms [pg. k5]
-runtime: O(n)
-"""
-
 import random
 
 
 def simple():
-
+	"""
+	* source: Grokking Algorithms [pg. 5]
+	* runtime: O(n)
+	"""
 	number_to_guess = random.randint(1, 100)
 	accumulated_guesses = 0
 
@@ -15,3 +13,25 @@ def simple():
 		accumulated_guesses = accumulated_guesses + 1
 		if i == number_to_guess:
 			return accumulated_guesses
+
+
+def binary(user_search, sorted_list):
+	"""
+	* source: Grokking Algorithms [pg. 6]
+	* runtime: O(log n)
+	* sketch: 3 placeholders (low, mid, high) in a list;
+	keep guessing the mid and reset low/high as necessary
+	"""
+	low_index = 0
+	high_index = len(sorted_list) - 1
+
+	while low_index <= high_index:
+		mid = (high_index + low_index) // 2
+		if sorted_list[mid] == user_search:
+			return 'found {}'.format(user_search)
+		elif sorted_list[mid] > user_search:
+			high_index = mid - 1
+		else:
+			low_index = mid + 1
+
+	return 'could not find {}'.format(user_search)
--- tests/test_search.py
+++ tests/test_search.py
@@ -8,5 +8,13 @@ class TestSearch(unittest.TestCase):
 	def test_simple(self):
 		self.assertGreaterEqual(100, search.simple())
 
-	def test_binary(self):
-		pass
+	def test_binary_happy(self):
+		query = 42
+		list_happy = [2, 13, 24, 35, 42, 57, 68, 79]
+		self.assertEqual('found 42', search.binary(query, list_happy))
+
+	def test_binary_unhappy(self):
+		query = 42
+		list_unhappy = [2, 13, 24, 35, 46, 57, 68, 79]
+		self.assertEqual('could not find 42', search.binary(query, list_unhappy))
+
