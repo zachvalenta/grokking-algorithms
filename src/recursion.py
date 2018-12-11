5e64b9ff9c1b9089f01043060de0c444c326ab8d
zachvalenta <zjv5002@gmail.com>
2018-12-11 15:19:29 -0500

    fix bug, update tests and docstrings

--- src/recursion.py
+++ src/recursion.py
@@ -1,4 +1,7 @@
 def countdown(arg):
+    """
+    * Grokking Algorithms pg. 41
+    """
     print(arg)
     return None if arg == 0 else countdown(arg - 1)
 
@@ -11,7 +14,10 @@ def do_sum(arg):
 
 
 def do_factorial(arg):
+    """
+    * Grokking Algorithms pg. 45
+    """
     if arg == 1:
         return arg
     else:
-        return arg * do_sum(arg - 1)
+        return arg * do_factorial(arg - 1)
--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -5,8 +5,9 @@ import src.recursion as r
 
 class TestRecursion(unittest.TestCase):
 
+    # TODO: parameterize https://stackoverflow.com/a/34094/6813490
     def test_do_sum(self):
-        self.assertEqual(r.do_sum(3), 6)
+        self.assertEqual(r.do_sum(4), 10)
 
     def test_do_factorial(self):
-        self.assertEqual(r.do_factorial(3), 9)
+        self.assertEqual(r.do_factorial(4), 24)
