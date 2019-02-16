9c77fafcd886cb53a0fc3ce87904dbd781ddd2f1
zachvalenta <zjv5002@gmail.com>
2019-02-16 14:39:51 -0500

    sort - selection - switch to list from dict

--- src/sort.py
+++ src/sort.py
@@ -1,21 +1,16 @@
-# TODO: `.keys()` should fix 'dictionary change size during iteration'
-# TODO: rf into single func - DefaultDict?
+def selection_sort(unsorted_list):
+    unsorted_list_len = len(unsorted_list)  # dont mutate list during loop
+    sorted_list = []
+    for _ in range(unsorted_list_len):
+        high = find_high_val(unsorted_list)
+        sorted_list.append(high)
+        unsorted_list.remove(high)
+    return sorted_list
 
 
-def selection_sort(user_dict):
-    sorted_dict = {}
-    for _ in range(len(user_dict)):
-        k, v = find_high_val(user_dict)
-        user_dict.pop(k)
-        sorted_dict[k] = v
-    return sorted_dict
-
-
-def find_high_val(dict_at_time):
-    high_key = ''
-    high_val = 0
-    for x in dict_at_time:
-        if dict_at_time[x] > high_val:
-            high_key = x
-            high_val = dict_at_time[x]
-    return high_key, high_val
+def find_high_val(unsorted_at_time):
+    high = 0
+    for ind, val in enumerate(unsorted_at_time):
+        if unsorted_at_time[ind] > high:
+            high = val
+    return high
--- test/test_sort.py
+++ test/test_sort.py
@@ -6,6 +6,4 @@ import src.sort as sort
 class TestSort(unittest.TestCase):
 
     def test_selection_sort(self):
-        artists_unsorted = {'blue nile': 47, 'prince': 14, 'ethereal': 50}
-        artists_sorted = {'ethereal': 50, 'blue nile': 47, 'prince': 14}
-        self.assertEqual(artists_sorted, sort.selection_sort(artists_unsorted))
+        self.assertEqual([50, 47, 14], sort.selection_sort([47, 14, 50]))
