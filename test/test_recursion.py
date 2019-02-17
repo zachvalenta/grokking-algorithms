9d81b26c7e394c7297d4dfa8922a81657f8237be
zachvalenta <zjv5002@gmail.com>
2019-02-17 16:46:00 -0500

    coverage to 100

--- test/test_recursion.py
+++ test/test_recursion.py
@@ -6,6 +6,9 @@ import src.recursion as r
 class TestRecursion(unittest.TestCase):
 
     # TODO: parameterize https://stackoverflow.com/a/34094/6813490
+    def test_countdown(self):
+        self.assertEqual(r.countdown(0), None)
+
     def test_do_sum(self):
         self.assertEqual(r.do_sum(4), 10)
 
