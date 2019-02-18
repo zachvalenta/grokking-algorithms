3d226417985f497d98fb8f89cf1a96573a6849c9
zachvalenta <zjv5002@gmail.com>
2019-02-18 14:38:13 -0500

    fix ordering in test assertions

--- test/test_recursion.py
+++ test/test_recursion.py
@@ -7,10 +7,10 @@ class TestRecursion(unittest.TestCase):
 
     # TODO: parameterize https://stackoverflow.com/a/34094/6813490
     def test_countdown(self):
-        self.assertEqual(r.countdown(0), None)
+        self.assertEqual(None, r.countdown(0))
 
     def test_do_sum(self):
-        self.assertEqual(r.do_sum(4), 10)
+        self.assertEqual(10, r.do_sum(4))
 
     def test_do_factorial(self):
-        self.assertEqual(r.do_factorial(4), 24)
+        self.assertEqual(24, r.do_factorial(4))
